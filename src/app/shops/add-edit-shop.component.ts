a0f79b33d260c885f29d5acc1a411fc0e1091c13
kityasis <asisjena03@gmial.com>
2020-02-02 16:49:03 +0530

    edit shop

--- src/app/shops/add-edit-shop.component.ts
+++ src/app/shops/add-edit-shop.component.ts
@@ -3,26 +3,36 @@ import { Shop } from '../model/shop';
 import { ShopService } from '../core/shop.service';
 import { Utils } from '../core/utils';
 import {Router} from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
-    selector: 'app-add-edit--shop',
+    selector: 'app-add-edit-shop',
     templateUrl: 'add-edit-shop.component.html',
     styleUrls: ['add-edit-shop.component.scss']    
 })
 
 export class AddEditShopComponent implements OnInit {
+    shopId: number;
     error: string;
     shop : Shop;
     constructor(private _shopService: ShopService,
-        private router: Router) { }
+        private _router: Router,
+        private _route: ActivatedRoute) { }
 
-    ngOnInit() {this.shop=new Shop(); }
+    ngOnInit() {
+        //this.shop=new Shop();
+        this.shopId = parseInt(this._route.snapshot.params.shopId);
+
+        this._shopService.getShop(this.shopId).subscribe(s => {
+          this.shop = s;        
+        }, error => (this.error = Utils.formatError(error)));
+     }
 
     
 
     addShop(shop) {
         this._shopService.addShop(shop).subscribe(shop => {
-            this.router.navigate(['/shops']); 
+            this._router.navigate(['/shops']); 
           }, error => this.error = Utils.formatError(error));
         // if (this.data.name) { this._dialogRef.close(this.data.name); }
         // else { this.error = "Please enter a name for the shop." };
